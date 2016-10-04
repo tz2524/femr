717f42cabca410742fee3608a4caf8aec8eec0a4
Tianxiang Zhang <TZhang@cometnet-10-21-34-121.utdallas.edu>
2016-10-04 17:13:43 -0500

    Add condition handle of the case that neither newPassword and newPasswordVerify is filled.

--- app/femr/ui/models/admin/users/EditViewModel.java
+++ app/femr/ui/models/admin/users/EditViewModel.java
@@ -46,15 +46,22 @@ public class EditViewModel {
             errors.add(new ValidationError("firstName", "first name is a required field"));
         if (StringUtils.isNullOrWhiteSpace(email))
             errors.add(new ValidationError("email", "email is a required field"));
-        if (!newPassword.equals(newPasswordVerify))
-            errors.add(new ValidationError("newPassword", "passwords do not match"));
-        else if(newPassword.isEmpty() || newPasswordVerify.isEmpty())
-            errors.add(new ValidationError("newPassword", "password field is empty"));
-        else {
-            if(newPassword.length() < 6 || !hasUppercase.matcher(newPassword).find()
-                    || !hasNumber.matcher(newPassword).find())      //AJ Saclayan Password Constraints
-                errors.add(new ValidationError("newPassword",
-                        "password must have at least 6 characters with at least one upper case letter and number"));
+
+
+
+        if( !(newPassword.isEmpty() && newPasswordVerify.isEmpty())){
+            if((newPassword.isEmpty() || newPasswordVerify.isEmpty()))
+                errors.add(new ValidationError("newPassword", "password field is empty"));
+            else if (!newPassword.equals(newPasswordVerify))
+                errors.add(new ValidationError("newPassword", "passwords do not match"));
+
+            else {
+
+                if(newPassword.length() < 6 || !hasUppercase.matcher(newPassword).find()
+                        || !hasNumber.matcher(newPassword).find())      //AJ Saclayan Password Constraints
+                    errors.add(new ValidationError("newPassword",
+                            "password must have at least 6 characters with at least one upper case letter and number"));
+            }
         }
         if (roles == null || roles.size() < 1)
             errors.add(new ValidationError("roles", "a user needs at least one role"));
